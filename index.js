53ad655e7c3b691465aa29bc82fe8857ab1acb6e
Gabriela Laryssa <gabrielaoliveiracet@gmail.com>
2022-07-06 21:30:22 -0300

    comentando o passo a passo

--- index.js
+++ index.js
@@ -1,45 +1,47 @@
 const express = require ('express');
+//pegando apenas a função engine do 'express-handlebars'
 const {engine} = require('express-handlebars');
 const mysql = require('mysql');
-
+const port = 3000
 const app = express()
-
+//configurando o express para resgatar dados do body
 app.use(
     express.urlencoded({
         extended: true
     })
 )
-
+//para pegar dados do body em formato json
 app.use(express.json())
-//console.log(exphbs.ExpressHandlebars())
 
+//configurando o handlebars
 app.engine('handlebars', engine());
-
 app.set('view engine', 'handlebars')
 
+//preparando para receber arquivos estaticos, um exemplo é o css
 app.use(express.static('public'))
 
+//preparando a renderização da rota
 app.get('/', (req, res)=>{
     res.render('home')
 })
 
+//rota para inserir dados dentro do banco de dados
 app.post('/books/insertbook', (req, res) => {
+    //a requisição é feita pelo name do form
     const title = req.body.title
     const idade = req.body.idade
-
     const sql = `INSERT INTO new_table (nome, idade) VALUES ('${title}','${idade}')`
-
+//executando a query sql
     conn.query(sql, function(err){
         if(err){
             console.log(err)
         }
-
+        //apos a realização da query a pagina é direcionada para a home
         res.redirect('/')
     })
 })
 
-//conexao mysql
-
+//conectando ao mysql
 const conn = mysql.createConnection({
     host: 'localhost',
     user: 'gabrieularyssa',
@@ -51,9 +53,7 @@ conn.connect(function(err){
     if(err){
         console.log(err)
     }
-
     console.log('Conectou ao mysql!!')
-
-    app.listen(3000)
+    app.listen(port)
 })
 
